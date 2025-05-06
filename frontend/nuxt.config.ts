fa8f117dd2dc3f4fca5609cb294176811fe03f51
Bloop <sp112131@gmail.com>
2025-05-06 21:51:22 +0800

    feat: integrate Tailwind CSS with Vite in Nuxt configuration and add main CSS file

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
 	modules: ['@nuxt/eslint'],
+	css: ['~/assets/css/main.css'],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
